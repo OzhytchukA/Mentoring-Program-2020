129d1e36e1a89b10fae6cf9e9352a84d34ae9412
Andrii_Ozhytchuk <Andrii_Ozhytchuk@epam.com>
2020-07-03 00:23:39 +0300

    Module 1[algorithms] - home task completed

--- module1/ArrayLeftRotation.php
+++ module1/ArrayLeftRotation.php
@@ -1,1 +1,27 @@
 <?php
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%[^\n]", $nd_temp);
+$nd = explode(' ', $nd_temp);
+
+$n = intval($nd[0]);
+
+$d = intval($nd[1]);
+
+fscanf($stdin, "%[^\n]", $a_temp);
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+/* PLEASE COPY THE CODE FRAGMENT AND ADD IT TO THE SITE */
+for ($i = 0; $i < $d; $i++) {
+	$firstElement = $a[$i];
+	unset($a[$i]);
+
+	$a[] = $firstElement;
+}
+
+echo implode(' ', $a);
+/* PLEASE COPY THE CODE FRAGMENT AND ADD IT TO THE SITE */
+
+fclose($stdin);
--- module1/DeleteDuplicateValue.php
+++ module1/DeleteDuplicateValue.php
@@ -1,1 +1,84 @@
 <?php
+
+class SinglyLinkedListNode
+{
+	public $data;
+	public $next;
+
+	public function __construct($node_data)
+	{
+		$this->data = $node_data;
+		$this->next = null;
+	}
+}
+
+class SinglyLinkedList
+{
+	public $head;
+	public $tail;
+
+	public function __construct()
+	{
+		$this->head = null;
+		$this->tail = null;
+	}
+
+	public function insertNode($node_data)
+	{
+		$node = new SinglyLinkedListNode($node_data);
+
+		if (is_null($this->head)) {
+			$this->head = $node;
+		} else {
+			$this->tail->next = $node;
+		}
+
+		$this->tail = $node;
+	}
+}
+
+function printSinglyLinkedList($node, $sep, $fptr)
+{
+	while (!is_null($node)) {
+		fwrite($fptr, $node->data);
+
+		$node = $node->next;
+
+		if (!is_null($node)) {
+			fwrite($fptr, $sep);
+		}
+	}
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+/* PLEASE COPY THE CODE FRAGMENT AND ADD IT TO THE SITE */
+function removeDuplicates($llist)
+{
+	$current = $llist;
+	$next = $llist->next;
+
+	while ($next !== null) {
+		if ($current->data === $next->data) {
+			$current->next = $next->next;
+		} else {
+			$current = $next;
+		}
+		$next = $next->next;
+	}
+
+	return $llist;
+}
+/* PLEASE COPY THE CODE FRAGMENT AND ADD IT TO THE SITE */
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
