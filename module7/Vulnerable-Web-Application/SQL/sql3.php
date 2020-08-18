b35cf7c2ca1a66514c1ff03c7b08f95ae391d0ad
Andrii_Ozhytchuk <Andrii_Ozhytchuk@epam.com>
2020-08-18 14:44:29 +0300

    Module 7[Security essentials] - part 2

--- module7/Vulnerable-Web-Application/SQL/sql3.php
+++ module7/Vulnerable-Web-Application/SQL/sql3.php
@@ -2,59 +2,59 @@
 <html>
 <head>
 	<title>SQL Injection</title>
-	<link rel="shortcut icon" href="../Resources/hmbct.png" />
+	<link rel="shortcut icon" href="../Resources/hmbct.png"/>
 </head>
 <body>
-
-	<div style="background-color:#c9c9c9;padding:15px;">
-      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
-      <button type="button" name="mainButton" onclick="location.href='sqlmainpage.html';">Main Page</button>
-	</div>
-
-	<div align="center">
-	<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" >
+<div style="background-color:#c9c9c9;padding:15px;">
+	<button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+	<button type="button" name="mainButton" onclick="location.href='sqlmainpage.html';">Main Page</button>
+</div>
+<div align="center">
+	<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
 		<p>Give me book's number and I give you book's name in my library.</p>
 		Book's number : <input type="text" name="number">
 		<input type="submit" name="submit" value="Submit">
 		<!-- <p>//Is this same with the level 2?</p>-->
 	</form>
-	</div>
+</div>
 
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "1ccb8097d0e9ce9f154608be60224c7c";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$db = "1ccb8097d0e9ce9f154608be60224c7c";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password,$db);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
 
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-	//echo "Connected successfully";
-	if(isset($_POST["submit"])){
-		$number = $_POST['number'];
-		$query = "SELECT bookname,authorname FROM books WHERE number = '$number'"; //Is this same with the level 2?
-		$result = mysqli_query($conn,$query);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+if (isset($_POST["submit"])) {
+	$number = intval($_POST['number']);
 
-		if (!$result) { //Check result
-		    $message  = 'Invalid query: ' . mysql_error() . "\n";
-		    $message .= 'Whole query: ' . $query;
-		    die($message);
-		}
+	$stmt = $conn->prepare('SELECT bookname, authorname FROM books WHERE number = ?');
+	$stmt->bind_param('d', $number);
+	$stmt->execute();
+	$result = $stmt->get_result();
 
-		while ($row = mysqli_fetch_assoc($result)) {
-			echo "<hr>";
-		    echo $row['bookname']." ----> ".$row['authorname'];    
-		}
-
-		if(mysqli_num_rows($result) <= 0)
-			echo "0 result";
-      
+	if (!$result) { //Check result
+		$message = 'Invalid query: ' . mysqli_error($conn) . "\n";
+		$message .= 'Whole query: ' . $stmt->get_warnings();
+		die($message);
 	}
-?> 
 
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo "<hr>";
+		echo $row['bookname'] . " ----> " . $row['authorname'];
+	}
+
+	if (mysqli_num_rows($result) <= 0)
+		echo "0 result";
+
+}
+?>
 </body>
 </html>
--- module7/Vulnerable-Web-Application/SQL/sql5.php
+++ module7/Vulnerable-Web-Application/SQL/sql5.php
@@ -2,16 +2,15 @@
 <html>
 <head>
 	<title>SQL Injection</title>
-	<link rel="shortcut icon" href="../Resources/hmbct.png" />
+	<link rel="shortcut icon" href="../Resources/hmbct.png"/>
 </head>
 <body>
-	<div style="background-color:#c9c9c9;padding:15px;">
-      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
-      <button type="button" name="mainButton" onclick="location.href='sqlmainpage.html';">Main Page</button>
-	</div>
-
-	<div align="center">
-	<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" >
+<div style="background-color:#c9c9c9;padding:15px;">
+	<button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+	<button type="button" name="mainButton" onclick="location.href='sqlmainpage.html';">Main Page</button>
+</div>
+<div align="center">
+	<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
 		<p>Give me book's number and I give you book's name in my library.</p>
 		Book's number : <input type="text" name="number">
 		<input type="submit" name="submit" value="Submit">
@@ -20,52 +19,52 @@
 			</p>
 		-->
 	</form>
-	</div>
+</div>
 
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "1ccb8097d0e9ce9f154608be60224c7c";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$db = "1ccb8097d0e9ce9f154608be60224c7c";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password,$db);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
 
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-	//echo "Connected successfully";
-	if(isset($_POST["submit"])){
-		$number = $_POST['number'];
-		//You hacked me again?
-		//I updated my code
-		if(strchr($number,"'")){
-			echo "What are you trying to do?<br>";
-			echo "Awesome hacking skillzz<br>";
-			echo "But you can't hack me anymore!";
-			exit;
-		}
-
-		$query = "SELECT bookname,authorname FROM books WHERE number =".'$number'; 
-		$result = mysqli_query($conn,$query);
-
-		if (!$result) { //Check result
-		    $message  = 'Invalid query: ' . mysql_error() . "\n";
-		    $message .= 'Whole query: ' . $query;
-		    die($message);
-		}
-
-		while ($row = mysqli_fetch_assoc($result)) {
-			echo "<hr>";
-		    echo $row['bookname']." ----> ".$row['authorname'];    
-		}
-
-		if(mysqli_num_rows($result) <= 0)
-			echo "0 result";
-
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+if (isset($_POST["submit"])) {
+	$number = $_POST['number'];
+	//You hacked me again?
+	//I updated my code
+	if (strchr($number, "'")) {
+		echo "What are you trying to do?<br>";
+		echo "Awesome hacking skillzz<br>";
+		echo "But you can't hack me anymore!";
+		exit;
 	}
-?> 
 
+	$stmt = $conn->prepare('SELECT bookname, authorname FROM books WHERE number = ?');
+	$stmt->bind_param('d', $number);
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if (!$result) { //Check result
+		$message = 'Invalid query: ' . mysqli_error($conn) . "\n";
+		$message .= 'Whole query: ' . $stmt->get_warnings();
+		die($message);
+	}
+
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo "<hr>";
+		echo $row['bookname'] . " ----> " . $row['authorname'];
+	}
+
+	if (mysqli_num_rows($result) <= 0)
+		echo "0 result";
+}
+?>
 </body>
 </html>
